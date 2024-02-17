d103a2a0e21a1eb25917e564b63aa66b9877d82d
GiovanniCortes <giocortes19@gmail.com>
2024-02-16 21:25:14 -0400

    Initialize file structure for game with Vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+  author: String,
+});
+</script>
+
+<template>
+  <div class="helloWorld">
+    <h1>Hello World: This is {{ author }}</h1>
+  </div>
+</template>
+
+<style>
+.helloWorld {
+  border: 1px solid green;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import WelcomeItem from '@/components/WelcomeItem.vue';
+</script>
+
+<template>
+  <div class="about">
+    <WelcomeItem />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    /* min-height: 100vh; */
+    display: flex;
+    align-items: center;
+    background-color: rgba(195, 195, 195, 0.778);
+  }
+}
+</style>
